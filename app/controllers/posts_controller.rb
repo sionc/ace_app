f88eec3d08c90435633ae909931df005911f6b00
sionc <sion.chaudhuri@gmail.com>
2012-07-23 00:17:19 -0700

    [Posts] Sort comments by created_at

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.all.sort_by &:created_at
 
     respond_to do |format|
       format.html # index.html.erb
