84befa44ca55dbecb94c3d4f601cf5b6b8e86929
sionc <sion.chaudhuri@gmail.com>
2012-09-26 14:24:02 -0700

    [PackExpo Demo] Setup webserver on a separate PC with MySQL
    -Changed database adapter settings so that we now use mysql
    -Updated readme

--- Gemfile
+++ Gemfile
@@ -5,8 +5,11 @@ gem 'rails', '3.2.5'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
+# gem for postgresql
 gem 'pg'
 
+# gem for mysql
+gem 'mysql2', '~> 0.3'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120717052959) do
+ActiveRecord::Schema.define(:version => 20120717045413) do
 
   create_table "posts", :force => true do |t|
     t.text     "comment"
